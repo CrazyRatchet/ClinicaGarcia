ee7b72313b75e2665c6269c4ed9f9b499a4d5bc1
Elena Wu <145410248+helen0908@users.noreply.github.com>
2024-10-07 22:01:06 -0500

    Validación

---
+++ ajax/validar_cedulaMedico.php
@@ -0,0 +1,18 @@
+<?php
+include '../config/Database.php';
+include '../includes/MedicosCredenciales.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$medicoscredenciales = new MedicosCredenciales($db);
+
+if (isset($_POST['cedula'])) {
+    $medicoscredenciales->cedula = $_POST['cedula'];
+    $resultado = $medicoscredenciales->buscar();
+
+    echo json_encode(['existe' => !empty($resultado)]);
+} else {
+    echo json_encode(['error' => 'No se proporcionó una cédula']);
+}
+?>
---
+++ ajax/validar_cedulaRecepcionista.php
@@ -0,0 +1,18 @@
+<?php
+include '../config/Database.php';
+include '../includes/RecepcionistasCredenciales.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$recepcionistascredenciales = new RecepcionistasCredenciales($db);
+
+if (isset($_POST['cedula'])) {
+    $recepcionistascredenciales->cedula = $_POST['cedula'];
+    $resultado = $recepcionistascredenciales->buscar();
+
+    echo json_encode(['existe' => !empty($resultado)]);
+} else {
+    echo json_encode(['error' => 'No se proporcionó una cédula']);
+}
+?>
